1980187b672556e9286ebe87fb03fce6ba30d722
catcherwong <catcher_hwq@outlook.com>
2023-04-16 16:51:16 +0800

    add command line custom value parser sample

---
+++ cli_cus_value_parser/CommandLineApiSample/Program.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json.Linq;
+using System.CommandLine;
+using System.CommandLine.Parsing;
+
+var vOption = new Option<List<Tuple<string, JToken>>>(
+    "-v",
+    description: "The variables.",
+    parseArgument: result =>
+    {
+        List<Tuple<string, JToken>> res = new();
+        foreach (var item in result.Tokens)
+        {
+            var fragments = item!.Value.Split('=');
+
+            try
+            {
+                // variable key
+                var variableKey = fragments[0].Trim();
+
+                // variable value, a json format value, such as json object, array, number, etc.
+                var variableValue = JToken.Parse(fragments[1].Trim());
+
+                res.Add(new Tuple<string, JToken>(variableKey, variableValue));
+            }
+            catch (Exception ex)
+            {
+                throw new FormatException(
+                    $"Invalid value specified for {result.Argument.Name}. '{item!.Value} is not a valid Tuple<string, JToken>",
+                    ex);
+            }
+        }
+       
+        return res;
+    });
+
+vOption.IsRequired = true;
+
+var rootCommand = new RootCommand("Parse variables")
+{
+    vOption
+};
+
+rootCommand.SetHandler((opList) =>
+{
+    foreach (var item in opList)
+    {
+        Console.WriteLine($"variable key:{item.Item1}");
+        Console.WriteLine($"variable value:{item.Item2}");
+        Console.WriteLine("====");
+    }
+}, vOption);
+
+await rootCommand.InvokeAsync(new string[] 
+{ 
+    "-v", "customHeaders=['accept-encoding: deflate', 'a: test']", 
+    "-v", "test={'a' : 'b', 'c' : 123}", 
+    "-v", "k=123", 
+    "-v", "k='abc'" 
+});
---
+++ cli_cus_value_parser/CommandLineUtilsSample/Program.cs
@@ -0,0 +1,59 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+using McMaster.Extensions.CommandLineUtils.Abstractions;
+using Newtonsoft.Json.Linq;
+using System.Globalization;
+
+var app = new CommandLineApplication();
+app.ValueParsers.Add(new VariableParser());
+app.HelpOption();
+var v = app.Option<Tuple<string, JToken>>("-v", "The variables ", CommandOptionType.MultipleValue);
+
+app.OnExecute(() =>
+{
+    foreach (var item in v.ParsedValues)
+    {
+        Console.WriteLine($"variable key:{item.Item1}");
+        Console.WriteLine($"variable value:{item.Item2}");
+        Console.WriteLine("====");
+    }
+});
+
+return app.Execute(new string[] 
+{ 
+    "-v", "customHeaders=['accept-encoding:deflate', 'a: test']", 
+    "-v", "test={'a':'b', 'c': 123}", 
+    "-v", "k=123", 
+    "-v", "k='abc'" 
+});
+
+public class VariableParser : IValueParser
+{
+    public Type TargetType { get; } = typeof(Tuple<string, JToken>);
+
+    public object? Parse(string? argName, string? value, CultureInfo culture)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            return default(Tuple<string, JToken>);
+        }
+
+        var fragments = value!.Split('=');
+
+        try
+        {
+            // variable key
+            var variableKey = fragments[0].Trim();
+
+            // variable value, a json format value, such as json object, array, number, etc.
+            var variableValue = JToken.Parse(fragments[1].Trim());
+
+            return new Tuple<string, JToken>(variableKey, variableValue);
+        }
+        catch (Exception ex)
+        {
+            throw new FormatException(
+                $"Invalid value specified for {argName}. '{value} is not a valid Tuple<string, JToken>",
+                ex);
+        }
+    }
+}
